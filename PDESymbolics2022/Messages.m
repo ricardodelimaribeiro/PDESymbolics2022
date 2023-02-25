966a5813ced22004d60108102083bc5207231d10
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2023-02-25 12:36:55 +0300

    Create Messages.m
    New messages package to deal with error or warning messages.

---
+++ PDESymbolics2022/Messages.m
@@ -0,0 +1,6 @@
+(* Wolfram Language package *)
+General::KeyAbsent = "Key `1` is missing. Proceeding with `2`.";
+Clear[keyAbsentMessage];
+keyAbsentMessage::usage = 
+"keyAbsentMessage[OperatorName][Key(s)][Standard Ouput] will print a the General::KeyAbsent message";
+keyAbsentMessage[op_][vars_] := (Message[op ::KeyAbsent, vars, #]; #) &
