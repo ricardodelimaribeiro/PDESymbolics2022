82e34b5e725573eb448fe037293b5f0049a7cd0a
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2023-03-16 15:43:13 +0300

    Update ParametricGroebnerBasis.m
    Dealing with facts.
    
    Generalizing SPolynomialOperator.
    
    Fixing Division.

--- PDESymbolics2022/ParametricGroebnerBasis.m
+++ PDESymbolics2022/ParametricGroebnerBasis.m
@@ -20,8 +20,10 @@ PiecewiseDivision::usage =
 "PiecewiseDivision[a,b] takes care of $Failed and division by zero."
 
 AutoReduceOperator::usage =
-"AutoReduceOperator[variables][polylist] returns the reduced polylist.";
+"AutoReduceOperator[variables][polylist] returns the reduced (smallest set generating the same ideal) polylist.";
 
+PiecewisePolynomialReduceRemainderOperator::usage =
+"PiecewisePolynomialReduceRemainderOperator[variables][p, polylist] returns a Piecewise remainder for the redeuctions.";
 (*Debuging*)
 SPolynomialOperator::usage="";
 Begin["`Private`"]
@@ -73,30 +75,42 @@ Clear[LeadingCoefficientOperator];
 LeadingCoefficient[variables_Association][xp_?NotPiecewise] := 
   Module[{order, generators, MonList, rules, facts},
    facts = Lookup[variables, "facts", True];
+   If[facts===False,$Failed,
+   	Assuming[facts,
    order = Lookup[variables, "ordering", "Lexicographic"];
    generators = 
     Lookup[variables, "generators", 
      InferGeneratorsOperator[variables][xp]];
    rules = # -> 1 & /@ generators;
    MonList = MonomialList[xp, generators, order];
-   MonList = Assuming[facts, Simplify/@(MonList /.rules)]; 
-   Piecewise[({#, Reduce[facts&&(# != 0)]} & /@ MonList),1](*//PiecewiseBeautify*) (*this was giving problems for a parametric leading coefficient (constant in the generators)*)
+   MonList = Simplify/@(MonList /.rules); 
+   (*Print["coeff list: ", MonList, " facts: ", facts];*)
+    PiecewiseBeautify@Simplify@Piecewise[({#, (*Reduce[*)facts&&(# != 0)(*]*)} & /@ MonList),1] (*this was giving problems for a parametric leading coefficient (constant in the generators)*)
+   ]
+   ]
    ];
 
 LeadingCoefficientOperator[vars_][xp_] := 
- KleisliListable[LeadingCoefficient][vars][xp]
-
+ KleisliListable[LeadingCoefficient][vars][xp](*//PiecewiseBeautify
+*)
 Division[a_?NotPiecewise,b_?NotPiecewise]:=
-Which[a === $Failed|| b === $Failed,
+If[a === $Failed|| b === $Failed,
 	$Failed,
-	a b === 0,
-	0,
-	True,
+	(*a b === 0,
+	0,*)
+	(*True,*)
 	a/b
 ];
 
 PiecewiseDivision[a_,b_]:= PiecewiseExpand[Division[a,b]];
 
+(*SPolynomialOperator[variables_][f_List]:=
+Module[{pairs},
+	pairs=Subsets[f,{2}];
+	Print[pairs];
+	SPolynomialOperator[variables][pairs]
+];*)
+Clear[SPolynomialOperator];
 SPolynomialOperator[variables_][f_?NotPiecewise,
 	g_?NotPiecewise] :=
 	Which[f === 0 || g === 0,
@@ -114,26 +128,83 @@ SPolynomialOperator[variables_][f_?NotPiecewise,
 				**********************************************************
 				", f," and ",g];
 				Assuming[facts,
-			Expand@PiecewiseEliminateEqualitiesOperator[variables]@PiecewiseBeautify@PiecewiseExpand[PiecewisePolynomialLCM[lf,lg](PiecewiseDivision[f,lf] - PiecewiseDivision[g,lg])](*//PiecewiseBeautify*)
+			Expand@(*PiecewiseEliminateEqualitiesOperator[variables]@*)PiecewiseBeautify@PiecewiseExpand[PiecewisePolynomialLCM[lf,lg](PiecewiseDivision[f,lf] - PiecewiseDivision[g,lg])](*//PiecewiseBeautify*)
 			],
-			Assuming[facts, Expand[PolynomialLCM[lf, lg] (f/lf - g/lg)]]
+			Assuming[facts, Expand[ReleaseHold[PolynomialLCM[lf, lg] Hold[(f/lf - g/lg)]]]]
 			]	
 			]
 		];
-
+Clear[PiecewiseSPolynomialOperator];
 PiecewiseSPolynomialOperator[variables_][f_, g_] := 
  FixedPoint[PiecewiseExpand, 
    SPolynomialOperator[variables][f, g]] // Expand;
 
-AutoReduceOperator[variables_][opolylist_] := 
+PiecewiseSPolynomialOperator[variables_][f_Piecewise] :=
+((*Print[piecewise];*)
+PiecewiseOperatorMap[PiecewiseSPolynomialOperator,variables, f]);
+
+PiecewiseSPolynomialOperator[variables_][f_List] :=
+Which[Length[f]===2,
+	(*Print[2];*)
+	{PiecewiseSPolynomialOperator[variables]@@f},
+	Length[f]>2,
+	Module[{pairs},
+		pairs = Subsets[f,{2}];
+		PiecewiseSPolynomialOperator[variables]@@@pairs
+		
+	],
+		True,
+		Print["PSPO with a longer list of size: "Length[f]];
+		$Failed
+];
+
+
+Clear[AutoReduceStepOperator];
+AutoReduceStepOperator[variables_][polylist_] := 
+  Kleisli[AutoReduceStep][variables][polylist];
+AutoReduceStep[variables_][polylist_List] :=
+  
+  With[{ps = sort@polylist},
+   (*Print[polylist];*)
+   Module[{unrefined},
+    unrefined = 
+     DeleteCases[0]@
+      Table[
+       PiecewisePolynomialReduceRemainderOperator[variables][ps[[i]], 
+        ps[[;; i - 1]]], {i, 1, Length@ps}];
+    PiecewiseDivision[unrefined, 
+     LeadingCoefficientOperator[variables]@unrefined]
+    ]
+   ];
+Clear[AutoReduceOperator];
+AutoReduceOperator[variables_][polylist_] := 
+  FixedPoint[AutoReduceStepOperator[variables], polylist] ;
+
+PPRRO[$Failed, _] = $Failed;
+PiecewisePolynomialReduceRemainderOperator[variables_][f_, g_List] := 
+  Module[{preliminary, generators}, 
+   generators = 
+    Lookup[variables, "generators", 
+     InferGeneratorsOperator[variables][Append[f][g]]]; 
+   preliminary = PiecewiseExpand[PPRRO[f, g]]; 
+   preliminary /. 
+    PPRRO -> 
+     Function[{poly, polylist}, 
+      Last[
+       PolynomialReduce[poly, DeleteCases[0]@polylist, generators]]]
+   ];
+(*AutoReduceOperator[variables_][opolylist_] := 
  With[{generators = Lookup[variables, "generators", {}],facts = Lookup[variables,"facts",True]},
-  Assuming[facts,DeleteDuplicates@FixedPoint[
+  Assuming[facts,(*DeleteDuplicates@*)FixedPoint[
    Function[polylist, 
+   	Module[{normalPolylist},
+   	normalPolylist=polylist;(*MapThread[PiecewiseDivision,{polylist,LeadingCoefficientOperator[variables]/@polylist}]//Expand;*)
+    Print[polylist];(*,normalPolylist];*)
     Select[
      Function[
        poly, (Last@
-         PolynomialReduce[poly, Select[polylist, # =!= poly &], 
-          generators])] /@ polylist, # =!= 0 &]], opolylist]]
-          ]; 
+         PolynomialReduce[poly, Select[normalPolylist, # =!= poly &], 
+          generators])] /@ normalPolylist, # =!= 0 &]]], opolylist]]
+  ]; *)
          
 End[]
