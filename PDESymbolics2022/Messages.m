2f080e7342bfc21ec905fe1ab0bf3746f4c6ec12
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2023-04-13 10:21:54 +0300

    Update Messages.m
    removed annoying messages

--- PDESymbolics2022/Messages.m
+++ PDESymbolics2022/Messages.m
@@ -3,4 +3,4 @@ General::KeyAbsent = "Key `1` is missing. Proceeding with `2`.";
 Clear[keyAbsentMessage];
 keyAbsentMessage::usage = 
 "keyAbsentMessage[OperatorName][Key(s)][Standard Ouput] will print a the General::KeyAbsent message";
-keyAbsentMessage[op_][vars_][std_] := (Message[op::KeyAbsent, vars, std]; std) 
+keyAbsentMessage[op_][vars_][std_] := ((*Message[op::KeyAbsent, vars, std];*) std) 
